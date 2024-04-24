5445ed16f02a2e59d50945e234b6ddbe1ef1ef4f
Sergio <sergiodmv239123@hotmail.com>
2024-04-23 21:22:24 -0600

    (backend) Add CORS policy to allow external hosts

--- packages/backend/Program.cs
+++ packages/backend/Program.cs
@@ -3,7 +3,17 @@ using backend.Models;
 using backend.Repository;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200");
+                      });
+});
 
 // Add services to the container.
 
@@ -28,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
